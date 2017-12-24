928176a4f37864a0ff19278867d1d7c8324aa660
subeshb1 <subeshb1@gmail.com>
2017-12-24 19:21:59 +0545

    Chat completed

--- app.js
+++ app.js
@@ -16,10 +16,32 @@ let io = socket(server);
 
 io.on('connection', function(socket) {
     console.log("Made socket connection",socket.id);
-    
+    let typers = [];
     socket.on('chat',function(data) {
         io.sockets.emit('chat',data);
         console.log(data.handle,data.message);
+        
+    });
+
+    socket.on('type',function(data) {
+        let index = typers.findIndex((item)=>  data.id==item.id);  
+        if(  index ==-1) {
+            typers.push(data);
+            io.sockets.emit('typer',typers);
+        }
+        
+    });
+
+    socket.on('removeType',function(data) {
+        let index = typers.findIndex((item)=>  data.id==item.id);  
+        if(  index !=-1) {
+            typers.splice(index,1);
+            io.sockets.emit('typer',typers);
+        }
+        
+        
+        
+      
         
     });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,24 +1,90 @@
 // Make Connsection
-var socket = io.connect('http://localhost:4000');
+let socket = io.connect('http://localhost:4000');
+let id;
+let typers = [];
+socket.on('connect', function () {
+    id = socket.io.engine.id;
+    console.log(id);
+
+})
 
 // Query dom
 let message = document.getElementById('message');
 let handle = document.getElementById('handle');
 let btn = document.getElementById('send');
 let output = document.getElementById('output');
+let type = document.getElementById('typer');
 
 //Emit events
 
-btn.addEventListener('click',function() {
-    socket.emit('chat',{
-        message: message.value,
-        handle: handle.value
-    })
+btn.addEventListener('click', function () {
+    if (message.value.length != 0 && handle.value.length != 0) {
+        socket.emit('chat', {
+            message: message.value,
+            handle: handle.value
+        });
+        message.value = "";
+    }
+    
+    
 });
 
 //Listen for events
-socket.on('chat',function(data) {
+socket.on('chat', function (data) {
     output.innerHTML += `
         <p><strong> ${data.handle} : </strong> ${data.message}</p>
     `;
 });
+
+
+socket.on('typer',function(data) {
+    typers = data;
+    addTyper();
+});
+message.addEventListener('keyup', function (e) {
+    
+     if (handle.value.length != 0) {
+        if(e.keyCode == 13 && message.value.length !=0 ) {
+            let event = new Event('click')
+            btn.dispatchEvent(event);
+            socket.emit('removeType', {
+                id: id,
+                handle: handle.value
+            });
+            
+        }
+        else if (this.value.length == 1) {
+            socket.emit('type', {
+                id: id,
+                handle: handle.value
+            });
+
+
+        } else if (this.value.length == 0) {
+            socket.emit('removeType', {
+                id: id,
+                handle: handle.value
+            });
+        }
+    }  
+
+});
+
+function addTyper() {
+    
+    let back = " is typing...";
+    let Person = [];
+    typers.forEach(element => {
+         if(element.id!=id) {
+            Person.push(element.handle)
+         }
+    });
+    if(Person.length!=0)  {
+    type.textContent = Person.join(',') + back;
+    }
+    else {
+        type.textContent="";
+    }
+    
+    
+}   
